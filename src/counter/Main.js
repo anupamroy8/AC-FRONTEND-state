99b4bcb4c942eb56802c3f07f33755152ba17fe7
anupamroy8 <anupamroy8@gmail.com>
2020-06-12 10:44:50 +0530

    counter completed

--- src/counter/Main.js
+++ src/counter/Main.js
@@ -1,5 +1,37 @@
 import React from 'react'
 
-export default function App() {
-  return <h1>Hello Counter</h1>
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    };
+  }
+  handleIncrement = (event) => {
+      console.log(event.nativeEvent)
+      console.log(this.state.count)
+      if(this.state.count<11){
+        this.setState({ count: this.state.count + 2 });
+      }else{
+        this.setState(null);
+      }      
+  };
+  handleDecrement = () =>{
+      this.setState({ count: this.state.count - 2 });  
+  };
+  handleReset = () => {
+      this.setState({ count:this.state.count = 0 })
+  };
+  render() {
+    return (
+      <div>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.handleIncrement}>Increment</button>
+        <button onClick={this.handleDecrement}>Decrement</button>
+        <button onClick={this.handleReset}>Reset</button>
+      </div>
+    );
+  }
 }
+
+export default App;
